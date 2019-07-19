4724900980d7b94823104a953ac9a47dd32a7a1d
Gravata-Prata <guimotapereira25@gmail.com>
2019-07-19 17:17:36 -0300

    Movimento do Player Adicionado
    Ainda sem animações

---
+++ Assets/Movimento.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public CharacterController2D controle;
+    public float velocidade = 40f;
+    float movHoriz = 0f;
+    bool pular = false;
+
+    void Update()
+    {
+        movHoriz = Input.GetAxisRaw("Horizontal") * velocidade;
+        if (Input.GetButtonDown("Jump"))
+        {
+            pular = true;
+        }
+    }
+
+    void FixedUpdate()
+    {   
+        controle.Move(movHoriz * Time.fixedDeltaTime, false, pular);
+        pular = false;
+    }
+
+}

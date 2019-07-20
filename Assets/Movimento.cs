b3706adc901b45a3220018eb79ef7a1451081292
Foot-G <gustavoplm@hotmail.com>
2019-07-19 22:06:05 -0300

    Adicionei as animações
    - Adicionadas animações de idle, walk e jump
    - Vai ser necessário dar uma arrumadinha nas animações pras elas ficaram perfeitas com relação ao movimento e a velocidade do movimento

--- Assets/Movimento.cs
+++ Assets/Movimento.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class Movimento : MonoBehaviour
 {
     public CharacterController2D controle;
+    public Animator animator;
+
     public float velocidade = 40f;
+
     float movHoriz = 0f;
     bool pular = false;
 
     void Update()
     {
         movHoriz = Input.GetAxisRaw("Horizontal") * velocidade;
+
+        animator.SetFloat("Speed", Mathf.Abs(movHoriz));
+
         if (Input.GetButtonDown("Jump"))
         {
             pular = true;
-            
+            animator.SetBool("IsJumping", true);
         }
         Debug.Log(Input.GetButtonDown("Jump"));
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -54,7 +54,7 @@ public class CharacterController2D : MonoBehaviour
 			if (colliders[i].gameObject != gameObject)
 			{
 				m_Grounded = true;
-				if (!wasGrounded)
+				if (!wasGrounded && m_Rigidbody2D.velocity.y < 0)
 					OnLandEvent.Invoke();
 			}
 		}

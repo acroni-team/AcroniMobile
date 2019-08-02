406a9bacdb903545f3efb806937b5112a6db5e39
Gravata-Prata <guimotapereira25@gmail.com>
2019-08-02 09:14:48 -0300

    Puzzle de memória Funcional
    Agora só falta adicionar as transições para mostrar as tiles para serem pisadas e as animações!

--- Assets/Scripts/StartShow.cs
+++ Assets/Scripts/StartShow.cs
@@ -1,31 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class StartShow : MonoBehaviour
 {
-    public GameObject player;
-    public Cinemachine.CinemachineVirtualCamera cam;
-    [Range(0, 1)]
-    public float velocity = 0.07f;
-    public Movimento player_mvt;
-
-    bool start = false;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.name.Equals("Ada"))
-        {
-            cam.Follow = player.transform;
-            start = true;
-            player_mvt.canMove = false;
-        }
-    }
-
-    void Update()
-    {
-        if (start)
-            player.transform.position = new Vector3(player.transform.position.x + velocity, player.transform.position.y, player.transform.position.z);
-    }
+    
 }
--- Assets/Scripts/SwitchFollow.cs
+++ Assets/Scripts/SwitchFollow.cs
@@ -1,17 +1,51 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 public class SwitchFollow : MonoBehaviour
 {
+    public ShowType type = ShowType.Start;
+
+    public PolygonCollider2D newBounds;
     public GameObject player;
     public Cinemachine.CinemachineVirtualCamera cam;
+    [Range(0, 1)]
+    public float velocity = 0.07f;
+    public Movimento player_mvt;
+    public Vector3 newTpLocal;
+
+    static bool movement_done = false;
+    bool start = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name);
-        if(collision.gameObject.CompareTag("showLevel"))
+        if (collision.gameObject.name.Equals("Ada") && type.Equals(ShowType.Start) && !movement_done)
         {
+            cam.GetComponent<CinemachineConfiner>().m_BoundingShape2D = newBounds;
+            player.transform.position = collision.transform.position;
             cam.Follow = player.transform;
-            Destroy(collision.gameObject);
+            start = true;
+            player_mvt.canMove = false;
         }
+        else if (collision.gameObject.CompareTag("showLevel") && type.Equals(ShowType.End) && !movement_done)
+        {
+            start = false;
+            movement_done = true;
+            player_mvt.canMove = true;
+            cam.Follow = player.transform;
+        }        
+        FindObjectOfType<Trigger>().tpLocal = newTpLocal;
+            
+    }
+
+    void Update()
+    {
+        if (start && !movement_done)
+            player.transform.position = new Vector3(player.transform.position.x + velocity, player.transform.position.y, player.transform.position.z);
+    }
+
+    public enum ShowType
+    {
+        Start,
+        End
     }
 }
